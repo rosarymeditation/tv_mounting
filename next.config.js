fd1a166a7cd3c95208e6cf5f2fdac39470de5bb2
Softnergy <softnergy@Softnergys-MacBook-Pro.local>
2025-08-22 07:23:31 +0100

    changed package.json

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-  target: "serverless",
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback.fs = false;
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
     }
     return config;
   },
 };
+
+module.exports = nextConfig;
